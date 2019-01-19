059c32964f0aad2cc3141f7239be0022419719c2
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-01-20 00:30:37 +0300

    Create setStatus.js

---
+++ GitHub-Tutorials/setStatus.js
@@ -0,0 +1,32 @@
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function setStatus(status, tutorial){
+  if (getCookie("tutorials") === ""){
+    let data13456 = {}
+    setCookie("tutorials", JSON.stringify(data13456), 9999)
+  }
+  var cookieData = getCookie("tutorials")
+  var parsedData = JSON.parse(cookieData)
+  eval("parsedData." + tutorial + " = " + status)
+  setCookie("tutorials", JSON.stringify(parsedData), 9999)
+ }
