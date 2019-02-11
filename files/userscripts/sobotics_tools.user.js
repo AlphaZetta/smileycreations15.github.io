ee4df2d29c3de0a607146b0410204ac85e98c27a
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-02-11 20:36:39 +0300

    commit

--- files/userscripts/sobotics_tools.user.js
+++ files/userscripts/sobotics_tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SOBotics Tools
 // @description  Tools for the SOBotics chatroom
-// @version      1.3.3
+// @version      1.3.4
 // @author       smileycreations15 (https://github.com/smileycreations15)
 // @match        https://chat.stackoverflow.com/rooms/111347/sobotics
 // @match        https://chat.stackoverflow.com/rooms/111347/sobotics?c=*&r=true
@@ -21,13 +21,13 @@
         .then(function(data) {
         if (GM_getValue("update-" + data.version) === "dismiss-true"){
         } else {
-            if (data.version !== "1.3.3"){
+            if (data.version !== "1.3.4"){
                 alert(data.updateMsg + " Click \"Install script update\" to install the update")
                 GM_setValue("update-" + data.version, "dismiss-true")
             }
         }
-            if (data.version !== "1.3.3"){
-                document.getElementById("smileycreations15-tools-buttons").innerHTML = document.getElementById("smileycreations15-tools-buttons").innerHTML + '<a href=\"https://github.com/smileycreations15/smileycreations15.github.io/raw/master/files/userscripts/sobotics_tools.user.js\" id=\"smileycreations15-tools-buttons-installUpdate\"><button class=\"button\" id=\"smileycreations15-tools-buttons-installUpdate-button\">Install script update<\/button><\/a>'
+            if (data.version !== "1.3.4"){
+                document.getElementById("smileycreations15-tools-buttons").innerHTML = document.getElementById("smileycreations15-tools-buttons").innerHTML + '<a href=\"https://github.com/smileycreations15/smileycreations15.github.io/raw/master/files/userscripts/sobotics_tools.user.js\" id=\"smileycreations15-tools-buttons-installUpdate\" target=\"_self\"><button class=\"button\" id=\"smileycreations15-tools-buttons-installUpdate-button\">Install script update<\/button><\/a>'
             }
     })
     // Natty report button
