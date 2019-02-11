e45295f252da808b6602035b8aa50a8fbe3e942b
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-02-11 18:28:34 +0300

    Update files

--- files/userscripts/sobotics_tools.user.js
+++ files/userscripts/sobotics_tools.user.js
@@ -15,27 +15,40 @@
 // ==/UserScript==
 
 (function() {
+    fetch("https://smileycreations15.github.io/files/text/sobotics_tools_userscript.json")
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function(data) {
+        if (GM_getValue("update-" + data.version) === "dismiss-true"){
+        } else {
+            if (data.version !== "1.1.6"){
+                if (confirm("A new version of SOBotics Tools is available! Press cancel to permanently dismiss.")){
+                } else {
+                    GM_setValue("update-" + data.version, "dismiss-true")
+                }
+            }
+        }
+    })
     // Natty report button
     document.getElementById("roomdesc").innerHTML = document.getElementById("roomdesc").innerHTML + '<div id=\"smileycreations15-tools\"><h3><br>Tools</h2><hr><div id=\"smileycreations15-tools-buttons\"></div>'
     document.getElementById("smileycreations15-tools-buttons").innerHTML = document.getElementById("smileycreations15-tools-buttons").innerHTML + '<a href=\"javascript:window.open(\'https:\/\/smileycreations15.github.io\/stackoverflow-stuff\/stackoverflow-report\',\'\', \'width = 700, height = 250\')\" id=\"smileycreations15-tools-buttons-reportToNatty\"><button class=\"button\" id=\"smileycreations15-tools-buttons-feedbackToNatty-button\">send answer feedback (Natty)<\/button><\/a>'
     document.getElementById("smileycreations15-tools-buttons").innerHTML = document.getElementById("smileycreations15-tools-buttons").innerHTML + '&nbsp;<a href=\"javascript:document.getElementById(\'input\').value = \'@Housekeeping open\';document.getElementById(\'sayit-button\').click()\" id=\"smileycreations15-tools-buttons-getNattyLinks\"><button class=\"button\" id=\"smileycreations15-tools-buttons-getNattyLinks-button\">get Natty links<\/button><\/a>'
     function getQueryVariable(variable){
-       var query = window.location.search.substring(1);
-       var vars = query.split("&")
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return decodeURIComponent(pair[1]);}
-       }
-       return(null);
-}
-    function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
+        var query = window.location.search.substring(1);
+        var vars = query.split("&")
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return decodeURIComponent(pair[1]);}
+        }
+        return(null);
     }
-  }
-}
+    function sleep(milliseconds) {
+        var start = new Date().getTime();
+        for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+                break;
+            }
+        }
+    }
 
     if (getQueryVariable("r") === "true"){
         const data = ["Sending request..."]
