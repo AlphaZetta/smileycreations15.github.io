4e41e21cc1382d230cc8ef083272bc2cbdd77784
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-02-16 23:02:36 +0300

    Delete php.php

--- php.php
+++
@@ -1,8 +0,0 @@
-<?php
-$image = imagecreate(300,200);
-$bg = imagecolorallocatealpha($image,255,255,255,0);
-$black = imagecolorallocate($image,255,255,255);
-imagettftext($image,100, 0, 0, 0, $black, "https://fonts.gstatic.com/s/opensans/v15/mem8YaGs126MiZpBA-UFVZ0bf8pkAg.woff2", "test");
-header('Content-Type: image/png');
-imagepng($image);
-?>
