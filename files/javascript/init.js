fe0d16a83a819346b485366adb569c66d5d3bef7
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-02-21 22:03:21 +0300

    add files

--- files/javascript/init.js
+++ files/javascript/init.js
@@ -42,7 +42,7 @@ if (null === getCookie("userID")){
 }
 /* ----------------------- */
 //This is the "Offline page" service worker
-
+/*
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
@@ -54,4 +54,6 @@ if (navigator.serviceWorker.controller) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
 }
+*/
 /* ----------------------- */
+
