843226f0f293b715998ffd5fcd1953383df2cf8f
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-03-02 17:40:42 +0300

    Update init.js

--- files/javascript/init.js
+++ files/javascript/init.js
@@ -57,3 +57,7 @@ if (navigator.serviceWorker.controller) {
 */
 /* ----------------------- */
 
+if(top!=self){
+    top.location.replace(document.location);
+    alert("For security reasons, framing is not allowed; click OK to remove the frames.")
+}
