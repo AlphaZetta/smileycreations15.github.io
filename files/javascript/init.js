ec6bc8950c260dbe8ee196b4ced48b9cd35c1845
smileycreations15 <45574030+smileycreations15@users.noreply.github.com>
2019-03-04 11:36:15 +0300

    Update init.js

--- files/javascript/init.js
+++ files/javascript/init.js
@@ -61,9 +61,7 @@ if (!(parent && parent.WebPlayer) && top != self) {
     top.location.replace(document.location);
     alert('For security reasons, framing is not allowed; click OK to remove the frames.');
 }
-/*
 if(top!=self){
     top.location.replace(document.location);
     alert("For security reasons, framing is not allowed; click OK to remove the frames.")
 }
-*/
