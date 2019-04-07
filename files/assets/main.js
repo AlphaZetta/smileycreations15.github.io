3dc28c6d81aa2692f8dec409f49dad85896b3934
smileycreations15 <mightyeagle75@yahoo.com>
2019-04-07 20:07:05 +0300

    Update main.js

--- files/assets/main.js
+++ files/assets/main.js
@@ -1,6 +1,7 @@
-dialogBox = function(location = "top-left",type = "plain"){
+dialogBox = function(location = "top-left",type = "plain",dialogContent){
   let dialog = document.createElement("div")
   dialog.className = "notify " + location + " do-show"
   dialog.notificationStatus = type
+  dialog.innerHTML = dialogContent
   document.body.appendChild(dialog)
 }
