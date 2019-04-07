aae39d105f54d710519177c8c088ed68fafbdbc7
smileycreations15 <mightyeagle75@yahoo.com>
2019-04-07 14:14:27 +0300

    .

--- files/javascript/pwa.js
+++ files/javascript/pwa.js
@@ -1,5 +1,5 @@
 function openPwaUrl(url){
   open(url,"","resizable=1,width=" + String(screen.width / 2) + ",height=" + String(screen.height / 2))
 }
-document.getElementById("error-2").style.display = "none"
-document.getElementById("error-1").style.display = "none"
+document.body.removeChild(document.getElementById("error-2"))
+document.body.removeChild(document.getElementById("error-1"))
