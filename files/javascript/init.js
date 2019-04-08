d8705e7bfe7110eb2768d72e3f0955505eb24d80
smileycreations15 <mightyeagle75@yahoo.com>
2019-04-08 08:27:41 +0300

    Update init.js

--- files/javascript/init.js
+++ files/javascript/init.js
@@ -146,3 +146,15 @@ if ("serviceWorker" in navigator) {
             document.getElementById("pwaProfile").style.display = ""
 		// sessionStorage.setItem("pwa","true")
         }
+
+dialogBox = function dialogBox(location = "top-left",type = "plain",dialogContent,black = true){
+  let dialog = document.createElement("div")
+  dialog.className = "notify " + location + " do-show font-notify"
+  dialog.dataset.notificationStatus = type
+  dialog.innerHTML = dialogContent
+  let blackText = ["success","notice","error","warning"]
+  if (blackText.includes(type) && black !== false){
+    dialog.style.color = "black"
+  }
+  document.body.appendChild(dialog)
+}
