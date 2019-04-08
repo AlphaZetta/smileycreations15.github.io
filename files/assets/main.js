d8a735036488b18be1abaf39b04e8444b10f7ded
smileycreations15 <mightyeagle75@yahoo.com>
2019-04-08 08:08:01 +0300

    Update main.js

--- files/assets/main.js
+++ files/assets/main.js
@@ -1,7 +1,11 @@
-dialogBox = function dialogBox(location = "top-left",type = "plain",dialogContent){
+dialogBox = function dialogBox(location = "top-left",type = "plain",dialogContent,black = true){
   let dialog = document.createElement("div")
   dialog.className = "notify " + location + " do-show font-notify"
   dialog.dataset.notificationStatus = type
   dialog.innerHTML = dialogContent
+  let blackText = ["info","success"]
+  if (blackText.includes(type) && black !== false){
+    dialog.style.color = "black"
+  }
   document.body.appendChild(dialog)
 }
