93acfbf3d3d450359d59679d2d86db3eeaff869d
smileycreations15 <mightyeagle75@yahoo.com>
2019-04-13 15:46:46 +0300

    Update init.js

--- files/javascript/init.js
+++ files/javascript/init.js
@@ -1,4 +1,7 @@
 /* ----------------------- */
+function openPwaUrl(url){
+	open(url,"","resizable=1,width=" + String(screen.width / 2) + ",height=" + String(screen.height / 2))
+}
 var savedPath = window.location.pathname
 function postSecure(data){
   if (null !== navigator.serviceWorker){
