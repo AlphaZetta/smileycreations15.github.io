82a135480ae115dff57a266e073c2d7200238ae7
smileycreations15 <git@smileycreations15.com>
2019-04-24 00:27:02 +0300

    Create build-3.js

---
+++ files/javascript/build-3.js
@@ -0,0 +1,12 @@
+try {
+    let e = window.location.hash.split("#")[window.location.hash.split("#").length - 1]
+    let jsonData1 = JSON.parse(e)
+    fetch("https://smileycreations15.wixsite.com/backend/_functions/github_create_page",{"method":"POST","body":JSON.stringify(
+        user: localStorage.getItem("github-username"),
+        token: localStorage.getItem("cookie-github"),
+        "404":jsonData1["404"],
+        "homepage":jsonData1["homepage"]
+    )}).then(function(){
+    window.location.pathname = "/website-builder/success"
+    })
+} catch(e){}
