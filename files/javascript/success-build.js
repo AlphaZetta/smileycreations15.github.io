e0c0405ada655f7aafa6fd82d5a59a657cf45102
smileycreations15 <git@smileycreations15.com>
2019-04-24 00:39:30 +0300

    Create success-build.js

---
+++ files/javascript/success-build.js
@@ -0,0 +1,4 @@
+document.getElementById("link-site").innerHTML = "https://" + localStorage.getItem("github-username") + ".github.io"
+document.getElementById("link-site").href = "https://" + localStorage.getItem("github-username") + ".github.io"
+document.getElementById("link-site-source").href = "https://github.com/" + localStorage.getItem("github-username") + "/" + localStorage.getItem("github-username") + ".github.io"
+document.getElementById("link-site-source").innerHTML = "https://github.com/" + localStorage.getItem("github-username") + "/" + localStorage.getItem("github-username") + ".github.io"
