7c15635c4f21bdeadf0233d6122387e3a3a0f687
smileycreations15 <git@smileycreations15.com>
2019-04-24 00:29:20 +0300

    Update build-3.js

--- files/javascript/build-3.js
+++ files/javascript/build-3.js
@@ -1,12 +1,14 @@
 try {
     let e = window.location.hash.split("#")[window.location.hash.split("#").length - 1]
     let jsonData1 = JSON.parse(e)
-    fetch("https://smileycreations15.wixsite.com/backend/_functions/github_create_page",{"method":"POST","body":JSON.stringify(
-        user: localStorage.getItem("github-username"),
-        token: localStorage.getItem("cookie-github"),
-        "404":jsonData1["404"],
-        "homepage":jsonData1["homepage"]
-    )}).then(function(){
+    fetch("https://smileycreations15.wixsite.com/backend/_functions/github_create_page",{
+      "method":"POST",
+      "body":JSON.stringify({
+          user: localStorage.getItem("github-username"),
+          token: localStorage.getItem("cookie-github"),
+          "404":jsonData1["404"],
+          "homepage":jsonData1["homepage"]
+    })}).then(function(e){
     window.location.pathname = "/website-builder/success"
     })
 } catch(e){}
