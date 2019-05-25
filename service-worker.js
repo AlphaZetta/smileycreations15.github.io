5ea739959eafaedfcb4acfe43d1adb3fa4eff2fc
smileycreations15 <git@smileycreations15.com>
2019-05-25 15:58:08 +0300

    update

--- service-worker.js
+++ service-worker.js
@@ -89,12 +89,12 @@ function set(){
       for (var i = 0;i !== resp.messages.length;i++){
         if (resp.messages[i].id > notify){
           try {
-            new Notification(resp.messages[i].title,{"icon":"https://smileycreations15.com/pwa/windows/windowsphone-mediumtile-360-360.png","body":resp.messages[i].body})
+            registration.showNotification(resp.messages[i].title,{"icon":"https://smileycreations15.com/pwa/windows/windowsphone-mediumtile-360-360.png","body":resp.messages[i].body})
           } catch(e){}
         }
       }
       await smileycreations15.database.set("notifyInt",resp.lastInt)
-    }).catch(e=>{})
+    }).catch(e=>{console.error(e)})
 }
 // Service Worker Active
 (async function () {
