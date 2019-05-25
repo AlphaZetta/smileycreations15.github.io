8c47c0096f842d5a8ec6ce8694e6627ff46d5ae0
smileycreations15 <git@smileycreations15.com>
2019-05-25 20:03:04 +0300

    update

--- service-worker.js
+++ service-worker.js
@@ -83,7 +83,7 @@ function updateCache(request, response) {
 }
 function set(){
   if (Notification.permission !== "granted") return;
-  return fetch("https://raw.githubusercontent.com/smileycreations15/misc-file-hosting/master/notify.json",{cache: "no-cache"})
+  return fetch("https://proxy-dikm7g5dt1fa.runkit.sh/notify.json",{cache: "no-cache"})
     .then(res=>{
         return res.json()
     })
